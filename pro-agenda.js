09c0e4b3ac58c853fe6aa47fde540134db62038f
Carl Burden <carl@summit-digital.co.uk>
2023-03-31 15:58:48 +0100

    Fix to ensure window height is correct on mobile browsers

---
+++ pro-agenda.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{var e=document.documentElement.clientHeight,t=$(".navigation-wrapper").height();$(document).ready(function(){$("#pro-agenda").css({"max-height":e-t+"px","min-height":e-t+"px"})});})();
